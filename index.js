804676002a1e96fc50fe19dc36b3fa1f046551a6
Chimereze Ikenna-Alfred <alfred.chimereze@gmail.com>
2018-07-23 09:26:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function add(a=4, b=5) {
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add(a, b) {
   var math = a + b;
   return math
 }
 add()
 
-function subtract(a=4, b=5) {
+function subtract(a, b) {
   var math = b - a;
   return math
 }
